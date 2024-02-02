611b86be7112a1739860f3d9271a38999012d1b9
nahcyrag <53359275+nahcyrag@users.noreply.github.com>
2024-02-01 20:52:49 -0500

    Update request.py

--- duboku/request.py
+++ duboku/request.py
@@ -37,8 +37,8 @@ class Request(object):
     def get(cls, path):
         url = 'https:/{}'.format(path)
         Log.info('[plugin.video.duboku] GET "{}"'.format(url))
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-response = cls.session.get(url, headers=headers)
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'} response = cls.session.get(url, headers=headers)
+
         if response.status_code == 200:
             return response.text
 
